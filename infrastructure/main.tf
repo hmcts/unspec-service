d70987b6b4fd82c711eb4e4b6cffa6ead706f8a8
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2020-06-12 09:46:13 +0100

    removed more deprecated expressions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,16 +6,16 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
 
-  tags = "${var.common_tags}"
+  tags = var.common_tags
 }
 
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "ucmc-${var.env}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
+  product                 = var.product
+  env                     = var.env
   tenant_id               = var.tenant_id
-  object_id               = "${var.jenkins_AAD_objectId}"
+  object_id               = var.jenkins_AAD_objectId
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_object_id = "294f60bc-f8a2-4be3-8bb8-84975444d4a0"
   common_tags             = var.common_tags
