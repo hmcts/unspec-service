ab62235c6a57865913f8b433bff4a61c8215ee1b
Sam Walker <samw@quinn.mynet>
2020-06-25 17:56:11 +0100

    removed user code

--- bin/add-roles-preview.sh
+++ bin/add-roles-preview.sh
@@ -8,20 +8,3 @@ for role in "${roles[@]}"
 do
   ./civil-unspecified-docker/bin/utils/ccd-add-role.sh "caseworker-cmc-${role}"
 done
-
-USER_EMAIL=solicitor@example.com
-FORENAME=Example
-SURNAME=Solicitor
-PASSWORD=Password12
-USER_ROLES='[{"code":"caseworker"},{"code":"caseworker-cmc-solicitor"},{"code":"caseworker-cmc"}]'
-
-
-curl -XPOST -H 'Content-Type: application/json' "${IDAM_API_BASE_URL:-http://localhost:5000}"/testing-support/accounts -d '{
-    "email": "'${USER_EMAIL}'",
-    "forename": "'${FORENAME}'",
-    "surname": "'${SURNAME}'",
-    "roles": '${USER_ROLES}',
-    "password": "'${PASSWORD}'"
-}'
-
-echo -e "\nCreated user with:\nUsername: ${USER_EMAIL}\nPassword: ${PASSWORD}\nFirstname: ${FORENAME}\nSurname: ${SURNAME}\nRoles: ${USER_ROLES}"
