97c472454819463c2abc76be77dee66a70bae935
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2020-06-30 15:45:30 +0100

    fixed pmd violation

--- src/main/java/uk/gov/hmcts/reform/ucmc/fnp/model/fee/FeeResponse.java
+++ src/main/java/uk/gov/hmcts/reform/ucmc/fnp/model/fee/FeeResponse.java
@@ -12,6 +12,6 @@ public class FeeResponse {
     private String code;
     private String description;
     private Integer version;
-    @JsonProperty(value = "fee_amount")
+    @JsonProperty("fee_amount")
     private BigDecimal amount;
 }
