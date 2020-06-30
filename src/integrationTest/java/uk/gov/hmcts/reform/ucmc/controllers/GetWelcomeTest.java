7f810a68af7981efe0f2e821fddc48f236715ca3
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2020-06-30 10:57:41 +0100

    fixed IT and reverted config changes

--- src/integrationTest/java/uk/gov/hmcts/reform/ucmc/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ucmc/controllers/GetWelcomeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -11,7 +12,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@ActiveProfiles("integration-test")
+@WebMvcTest(RootController.class)
 public class GetWelcomeTest {
 
     @Autowired
--- src/main/java/uk/gov/hmcts/reform/ucmc/Application.java
+++ src/main/java/uk/gov/hmcts/reform/ucmc/Application.java
@@ -3,11 +3,9 @@ package uk.gov.hmcts.reform.ucmc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = "uk.gov.hmcts")
+@SpringBootApplication
 @EnableCircuitBreaker
-@EnableFeignClients
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
