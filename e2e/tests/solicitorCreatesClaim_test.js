d4f85aeb8798efc420031842626151411ea76702
Sam Walker <samw@quinn.mynet>
2020-07-01 10:54:43 +0100

    test: configured e2e test for create claim journey

---
+++ e2e/tests/solicitorCreatesClaim_test.js
@@ -0,0 +1,9 @@
+const config = require('../config.js');
+
+Feature('Claim creation @create-claim');
+
+Scenario('Solicitor creates claim', async (I) => {
+  await I.login(config.solicitorUser);
+  await I.createCase();
+  I.seeElement(locate('exui-alert').withText('created'));
+});
