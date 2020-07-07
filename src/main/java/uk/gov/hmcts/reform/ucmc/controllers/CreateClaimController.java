851200331dd85919878c528555b0419c2627a5fc
Sam Walker <samw@quinn.mynet>
2020-07-07 11:38:09 +0100

    added claim value validation and html link for court finder

--- src/main/java/uk/gov/hmcts/reform/ucmc/controllers/CreateClaimController.java
+++ src/main/java/uk/gov/hmcts/reform/ucmc/controllers/CreateClaimController.java
@@ -1,13 +1,22 @@
 package uk.gov.hmcts.reform.ucmc.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.ucmc.model.ClaimValue;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static java.lang.String.format;
 import static uk.gov.hmcts.reform.ucmc.helpers.DateFormatHelper.DATE_TIME_AT;
@@ -17,6 +26,25 @@ import static uk.gov.hmcts.reform.ucmc.helpers.DateFormatHelper.formatLocalDateT
 @RestController
 @RequestMapping("/create-claim")
 public class CreateClaimController {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @PostMapping("/mid-event")
+    public AboutToStartOrSubmitCallbackResponse handleMid(@RequestBody CallbackRequest request) {
+        Map<String, Object> data = request.getCaseDetails().getData();
+        ClaimValue claimValue = mapper.convertValue(data.get("claimValue"), ClaimValue.class);
+        List<String> errors = new ArrayList<>();
+
+        if (claimValue.hasLargerLowerValue()) {
+            errors.add("CONTENT TBC: Higher value must not be lower than the lower value.");
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(data)
+            .errors(errors)
+            .build();
+    }
 
     @PostMapping("/submitted")
     public SubmittedCallbackResponse handleSubmitted() {
