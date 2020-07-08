3c77df2523b02a46bb9fff8f5a7ce612af4f3c76
Sabah Irfan <sabah.irfan@gmail.com>
2020-07-08 19:01:17 +0100

    Fix integration tests with security enabled

--- src/integrationTest/java/uk/gov/hmcts/reform/ucmc/controllers/BaseIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ucmc/controllers/BaseIntegrationTest.java
@@ -3,21 +3,41 @@ package uk.gov.hmcts.reform.ucmc.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 import uk.gov.hmcts.reform.ucmc.Application;
+import uk.gov.hmcts.reform.ucmc.TestIdamConfiguration;
 import uk.gov.hmcts.reform.ucmc.service.UserService;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.google.common.collect.ImmutableList.of;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest(
-    classes = Application.class
+    classes = {Application.class, TestIdamConfiguration.class}
 )
 @AutoConfigureMockMvc
 @TestPropertySource(
@@ -40,6 +60,11 @@ public abstract class BaseIntegrationTest {
         + "k02vLJDY9fLCsFYy5iWGCjb8lD1aX1NTv7jz2ttNNv7-smqp6L3LSSD_LCZMpf0h_3n5RXiv-N3vNpWe4ZC9u0AWQdHEE9QlKTZlsqwKSog"
         + "3yJWhyxAamdMepgW7Z8jQ";
 
+    private static final UserInfo USER_INFO = UserInfo.builder()
+        .sub("solicitor@example.com")
+        .roles(of("caseworker-cmc-solicitor"))
+        .build();
+
     @Autowired
     protected ObjectMapper objectMapper;
 
@@ -48,6 +73,45 @@ public abstract class BaseIntegrationTest {
 
     @MockBean
     protected UserService userService;
+
+    @MockBean
+    protected Authentication authentication;
+    @MockBean
+    protected SecurityContext securityContext;
+    @MockBean
+    protected JwtDecoder jwtDecoder;
+
+    @BeforeEach
+    public void setUpBase() {
+        when(userService.getUserInfo(anyString())).thenReturn(USER_INFO);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        setSecurityAuthorities(authentication);
+        when(jwtDecoder.decode(anyString())).thenReturn(getJwt());
+    }
+
+    protected void setSecurityAuthorities(Authentication authenticationMock, String... authorities) {
+        when(authenticationMock.getPrincipal()).thenReturn(getJwt());
+
+        Collection<? extends GrantedAuthority> authorityCollection = Stream.of(authorities)
+            .map(a -> new SimpleGrantedAuthority(a))
+            .collect(Collectors.toCollection(ArrayList::new));
+
+        when(authenticationMock.getAuthorities()).thenAnswer(invocationOnMock -> authorityCollection);
+    }
+
+    private Jwt getJwt() {
+        return Jwt.withTokenValue(BEARER_TOKEN)
+            .claim("exp", Instant.ofEpochSecond(1585763216))
+            .claim("iat", Instant.ofEpochSecond(1585734416))
+            .claim("token_type", "Bearer")
+            .claim("tokenName", "access_token")
+            .claim("expires_in", 28800)
+            .header("kid", "b/O6OvVv1+y+WgrH5Ui9WTioLt0=")
+            .header("typ", "RS256")
+            .header("alg", "RS256")
+            .build();
+    }
 
     @SneakyThrows
     protected <T> ResultActions doPost(String auth, T content, String urlTemplate, Object... uriVars) {
--- src/integrationTest/java/uk/gov/hmcts/reform/ucmc/controllers/CallbackControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ucmc/controllers/CallbackControllerTest.java
@@ -1,27 +1,17 @@
 package uk.gov.hmcts.reform.ucmc.controllers;
 
-import com.google.common.collect.ImmutableList;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 import uk.gov.hmcts.reform.ucmc.callback.CaseEvent;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.ucmc.callback.CallbackType.ABOUT_TO_START;
 
 public class CallbackControllerTest extends BaseIntegrationTest {
 
     private static final String CALLBACK_URL = "/cases/callbacks/{callback-type}";
-    private static final UserInfo USER_INFO = UserInfo.builder().roles(ImmutableList.of("caseworker-cmc")).build();
-
-    @BeforeEach
-    public void setUp() {
-        given(userService.getUserInfo(BEARER_TOKEN)).willReturn(USER_INFO);
-    }
 
     @Test
     @SneakyThrows
