b57fec15ad658f0ae491206b63188de2b64cf981
Christophe Kalista <christophe.kalista@gmail.com>
2020-07-14 23:43:47 +0100

    fix: make lambok builder play nicely with Jackon mapper

--- src/main/java/uk/gov/hmcts/reform/ucmc/model/ClaimValue.java
+++ src/main/java/uk/gov/hmcts/reform/ucmc/model/ClaimValue.java
@@ -2,14 +2,16 @@ package uk.gov.hmcts.reform.ucmc.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
 @RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class ClaimValue {
-    private final Integer lowerValue;
-    private final Integer higherValue;
+    private  final Integer lowerValue;
+    private  final Integer higherValue;
 
     public boolean hasLargerLowerValue() {
         if (lowerValue == null || higherValue == null) {
