d0620aa9087567b2cda67bcbc6f0fa09ede93b9e
Sam Walker <samw@quinn.mynet>
2020-07-15 14:14:11 +0100

    added auth provider configuration

--- src/main/java/uk/gov/hmcts/reform/ucmc/handler/MoveCaseToStayedEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/ucmc/handler/MoveCaseToStayedEventHandler.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ucmc.event.MoveCaseToStayedEvent;
 import uk.gov.hmcts.reform.ucmc.service.CoreCaseDataService;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class MoveCaseToStayedEventHandler {
     private final CoreCaseDataService coreCaseDataService;
