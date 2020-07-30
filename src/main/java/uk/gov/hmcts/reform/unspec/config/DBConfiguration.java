efaa3b20016a44d0e167a8442651ee1927691096
Dharmendra kumar <k_dharmendra@hotmail.com>
2020-07-30 17:59:23 +0100

    merge master changes

--- src/main/java/uk/gov/hmcts/reform/unspec/config/DBConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/config/DBConfiguration.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.unspec.config;
 
+import org.flywaydb.core.Flyway;
 import org.skife.jdbi.v2.DBI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +13,12 @@ import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.transaction.PlatformTransactionManager;
 import uk.gov.hmcts.reform.unspec.repositories.ReferenceNumberRepository;
 
-
 import javax.sql.DataSource;
 
 @Configuration
 public class DBConfiguration {
+    @Value("${database.migration}")
+    public boolean databaseMigration;
 
     @Bean
     @Primary
@@ -32,7 +35,11 @@ public class DBConfiguration {
 
     @Bean
     public TransactionAwareDataSourceProxy transactionAwareDataSourceProxy(DataSource dataSource) {
-        return new TransactionAwareDataSourceProxy(dataSource);
+        TransactionAwareDataSourceProxy dataSourceProxy = new TransactionAwareDataSourceProxy(dataSource);
+        if (databaseMigration) {
+            migrateFlyway(dataSourceProxy);
+        }
+        return dataSourceProxy;
     }
 
     @Bean
@@ -40,6 +47,15 @@ public class DBConfiguration {
         TransactionAwareDataSourceProxy transactionAwareDataSourceProxy
     ) {
         return new DataSourceTransactionManager(transactionAwareDataSourceProxy);
+    }
+
+
+    private void migrateFlyway(DataSource dataSource) {
+        Flyway.configure()
+            .dataSource(dataSource)
+            .locations("/")
+            .load()
+            .migrate();
     }
 
     @Bean
