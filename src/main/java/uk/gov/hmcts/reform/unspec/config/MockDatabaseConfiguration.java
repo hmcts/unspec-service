2920102132ffce3025234c9e2200e23d6577d8ca
Dharmendra kumar <k_dharmendra@hotmail.com>
2020-07-31 11:57:58 +0100

    resolve impoort issues

--- src/main/java/uk/gov/hmcts/reform/unspec/config/MockDatabaseConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/config/MockDatabaseConfiguration.java
@@ -1,20 +1,9 @@
 package uk.gov.hmcts.reform.unspec.config;
 
-import org.flywaydb.core.Flyway;
-import org.skife.jdbi.v2.DBI;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
-import org.springframework.transaction.PlatformTransactionManager;
 import uk.gov.hmcts.reform.unspec.repositories.ReferenceNumberRepository;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ConditionalOnProperty(value = "database.migration.enabled", havingValue = "false")
