d4d4a70e9ffb980d5a3d25304a81ff5b62d1177a
Sam Walker <samw@kainos.com>
2020-09-01 09:59:04 +0100

    added rest call to confirm service about to start callback

--- src/main/java/uk/gov/hmcts/reform/unspec/Application.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/Application.java
@@ -1,13 +1,19 @@
 package uk.gov.hmcts.reform.unspec;
 
+import org.camunda.bpm.extension.rest.EnableCamundaRestClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCamundaRestClient
 @EnableCircuitBreaker
-@EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.idam.client", "uk.gov.hmcts.reform.unspec"})
+@EnableFeignClients(basePackages = {
+    "uk.gov.hmcts.reform.idam.client",
+    "uk.gov.hmcts.reform.unspec",
+    "uk.gov.hmcts.reform.camunda"
+})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
