7e8bfe22301231e7e6245767f27ec82ec7f139f9
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2020-10-09 15:49:13 +0100

    updated with hasNoOngoingBusinessProcess method

--- src/main/java/uk/gov/hmcts/reform/unspec/service/BusinessProcessService.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/service/BusinessProcessService.java
@@ -5,11 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.unspec.callback.CaseEvent;
 import uk.gov.hmcts.reform.unspec.model.BusinessProcess;
+import uk.gov.hmcts.reform.unspec.model.CaseData;
 
 import java.util.List;
 import java.util.Map;
 
-import static uk.gov.hmcts.reform.unspec.enums.BusinessProcessStatus.FINISHED;
 import static uk.gov.hmcts.reform.unspec.enums.BusinessProcessStatus.READY;
 
 @Service
@@ -21,19 +21,11 @@ public class BusinessProcessService {
     private final ObjectMapper mapper;
 
     public List<String> updateBusinessProcess(Map<String, Object> data, CaseEvent caseEvent) {
-        BusinessProcess businessProcess = mapper.convertValue(data.get("businessProcess"), BusinessProcess.class);
-        if (hasNoOngoingBusinessProcess(businessProcess)) {
+        CaseData caseData = mapper.convertValue(data, CaseData.class);
+        if (caseData.hasNoOngoingBusinessProcess()) {
             data.put("businessProcess", BusinessProcess.builder().camundaEvent(caseEvent.name()).status(READY).build());
             return List.of();
         }
         return List.of(ERROR_MESSAGE);
-    }
-
-    //TODO: extract once merged:
-    //      https://github.com/hmcts/unspec-service/pull/242/files#diff-4c2ee2ed8f256169482f5b867f57ea2dR53
-    private boolean hasNoOngoingBusinessProcess(BusinessProcess businessProcess) {
-        return businessProcess == null
-            || businessProcess.getStatus() == null
-            || businessProcess.getStatus() == FINISHED;
     }
 }
