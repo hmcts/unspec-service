9940181b03aeca930601cda5229298e190262bb9
Sam Walker <samw@kainos.com>
2020-10-26 16:03:57 +0000

    feat: added service auth header to camunda client (CMC-932)

--- src/main/java/uk/gov/hmcts/reform/unspec/config/ExternalTaskListenerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/config/ExternalTaskListenerConfiguration.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.reform.unspec.config;
 
 import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.interceptor.ClientRequestContext;
+import org.camunda.bpm.client.interceptor.ClientRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 @Configuration
-public class ExternalTaskListenerConfiguration {
+public abstract class ExternalTaskListenerConfiguration implements AuthTokenGenerator {
 
     private final String baseUrl;
 
@@ -17,7 +21,16 @@ public class ExternalTaskListenerConfiguration {
     @Bean
     public ExternalTaskClient client() {
         return ExternalTaskClient.create()
+            .addInterceptor(new ServiceAuthProvider())
             .baseUrl(baseUrl)
             .build();
+    }
+
+    public class ServiceAuthProvider implements ClientRequestInterceptor {
+
+        @Override
+        public void intercept(ClientRequestContext requestContext) {
+            requestContext.addHeader(ServiceAuthFilter.AUTHORISATION, generate());
+        }
     }
 }
