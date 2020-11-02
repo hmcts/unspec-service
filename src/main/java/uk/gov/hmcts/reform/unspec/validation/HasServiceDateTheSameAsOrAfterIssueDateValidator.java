c99b7928ad41c2294f2f04caa8df3c730293c747
Sabah Irfan <sabah.irfan@gmail.com>
2020-11-02 11:23:25 +0000

    Validate deemed date of service with claim issued date

--- src/main/java/uk/gov/hmcts/reform/unspec/validation/HasServiceDateTheSameAsOrAfterIssueDateValidator.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/validation/HasServiceDateTheSameAsOrAfterIssueDateValidator.java
@@ -1,24 +1,35 @@
 package uk.gov.hmcts.reform.unspec.validation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.unspec.model.CaseData;
+import uk.gov.hmcts.reform.unspec.model.ServiceMethod;
+import uk.gov.hmcts.reform.unspec.service.DeadlinesCalculator;
 import uk.gov.hmcts.reform.unspec.validation.interfaces.HasServiceDateTheSameAsOrAfterIssueDate;
 
 import java.time.LocalDate;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+@Component
+@RequiredArgsConstructor
 public class HasServiceDateTheSameAsOrAfterIssueDateValidator implements
     ConstraintValidator<HasServiceDateTheSameAsOrAfterIssueDate, CaseData> {
 
-    @Override
-    public boolean isValid(CaseData value, ConstraintValidatorContext context) {
-        LocalDate serviceDate;
-        if (value.getServiceMethodToRespondentSolicitor1().requiresDateEntry()) {
-            serviceDate = value.getServiceDateToRespondentSolicitor1();
-        } else {
-            serviceDate = value.getServiceDateTimeToRespondentSolicitor1().toLocalDate();
-        }
+    private final DeadlinesCalculator deadlinesCalculator;
 
-        return serviceDate.equals(value.getClaimIssuedDate()) || serviceDate.isAfter(value.getClaimIssuedDate());
+    @Override
+    public boolean isValid(CaseData caseData, ConstraintValidatorContext context) {
+        LocalDate serviceDate;
+        ServiceMethod serviceMethod = caseData.getServiceMethodToRespondentSolicitor1();
+        if (serviceMethod.requiresDateEntry()) {
+            serviceDate = caseData.getServiceDateToRespondentSolicitor1();
+        } else {
+            serviceDate = caseData.getServiceDateTimeToRespondentSolicitor1().toLocalDate();
+        }
+        LocalDate deemedDateOfService = deadlinesCalculator.calculateDeemedDateOfService(
+            serviceDate, serviceMethod.getType());
+        return deemedDateOfService.equals(caseData.getClaimIssuedDate())
+            || deemedDateOfService.isAfter(caseData.getClaimIssuedDate());
     }
 }
