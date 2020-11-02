706750b31bd3271e6cfe52611a409c9638b25c16
Sam Walker <samw@kainos.com>
2020-11-02 15:33:32 +0000

    feat: defendant litigation friend event (CMC-930)

--- e2e/tests/solicitorCreatesClaim_test.js
+++ e2e/tests/solicitorCreatesClaim_test.js
@@ -13,32 +13,37 @@ Scenario('Solicitor creates claim @create-claim', async (I) => {
   await I.see(`Case ${caseNumber.split('-').join('')} has been created.`);
 });
 
-Scenario('Solicitor confirms service', async (I) => {
+xScenario('Solicitor confirms service', async (I) => {
   await I.confirmService();
   await I.see(caseEventMessage('Confirm service'));
 });
 
-Scenario('Solicitor acknowledges service', async (I) => {
+xScenario('Solicitor acknowledges service', async (I) => {
   await I.acknowledgeService();
   await I.see(caseEventMessage('Acknowledge service'));
 });
 
-Scenario('Solicitor requests extension', async (I) => {
+xScenario('Solicitor requests extension', async (I) => {
   await I.requestExtension();
   await I.see(caseEventMessage('Request extension'));
 });
 
-Scenario('Solicitor reponds to extension request', async (I) => {
+xScenario('Solicitor reponds to extension request', async (I) => {
   await I.respondToExtension();
   await I.see(caseEventMessage('Respond to extension request'));
 });
 
-Scenario('Solicitor responds to claim', async (I) => {
+xScenario('Solicitor responds to claim', async (I) => {
   await I.respondToClaim();
   await I.see(caseEventMessage('Respond to claim'));
 });
 
-Scenario('Solicitor responds to defence', async (I) => {
+xScenario('Solicitor responds to defence', async (I) => {
   await I.respondToDefence();
   await I.see(caseEventMessage('View and respond to defence'));
+});
+
+Scenario('Solicitor adds defendant litigation friend', async (I) => {
+  await I.addDefendantLitigationFriend();
+  await I.see(caseEventMessage('Add litigation friend'));
 });
