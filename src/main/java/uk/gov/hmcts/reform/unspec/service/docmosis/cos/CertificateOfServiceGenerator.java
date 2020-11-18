d635075e64f159a638db9b306cc7e9b44e138eab
Sabah Irfan <sabah.irfan@gmail.com>
2020-11-18 12:27:56 +0000

    Add additional field in COS document

--- src/main/java/uk/gov/hmcts/reform/unspec/service/docmosis/cos/CertificateOfServiceGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/service/docmosis/cos/CertificateOfServiceGenerator.java
@@ -73,6 +73,7 @@ public class CertificateOfServiceGenerator implements TemplateDataGenerator<Cert
             .respondentName(CaseNameUtils.fetchRespondentName(caseData))
             .respondentRepresentative(TEMP_REPRESENTATIVE)
             .serviceMethod(caseData.getServiceMethodToRespondentSolicitor1().getType().getLabel())
+            .onWhomServed(caseData.getServiceNamedPersonToRespondentSolicitor1())
             .servedLocation(prepareServedLocation(caseData.getServiceLocationToRespondentSolicitor1()))
             .documentsServed(prepareDocumentList(caseData.getServedDocuments(), caseData.getServedDocumentsOther()))
             .statementOfTruth(caseData.getApplicantSolicitor1ClaimStatementOfTruth())
