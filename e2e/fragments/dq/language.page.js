65e2ebcb69d9a832b72b7660fccf51cd8100fdc5
Sam Walker <samw@kainos.com>
2020-11-30 13:15:49 +0000

    reworked dq lanaguage selections in e2e

--- e2e/fragments/dq/language.page.js
+++ e2e/fragments/dq/language.page.js
@@ -14,25 +14,19 @@ module.exports = {
       evidence: {
         id: `#${party}DQLanguage_evidence`,
         options: {
-          welsh: 'Welsh',
-          english: 'English',
-          both: 'Welsh and english'
+          welsh: `#${party}DQLanguage_evidence-WELSH`,
         }
       },
       court: {
         id: `#${party}DQLanguage_court`,
         options: {
-          welsh: 'Welsh',
-          english: 'English',
-          both: 'Welsh and english'
+          welsh: `#${party}DQLanguage_court-WELSH`,
         }
       },
       documents: {
         id: `#${party}DQLanguage_documents`,
         options: {
-          welsh: 'Welsh',
-          english: 'English',
-          both: 'Welsh and english'
+          welsh: `#${party}DQLanguage_documents-WELSH`,
         }
       },
     };
@@ -44,19 +38,10 @@ module.exports = {
       I.click(this.fields(party).isPartyWelsh.options.yes);
     });
 
-    I.waitForElement(this.fields(party).evidence.id);
-    await within(this.fields(party).evidence.id, () => {
-      I.click(this.fields(party).evidence.options.welsh);
-    });
+    I.click(this.fields(party).evidence.options.welsh);
+    I.click(this.fields(party).court.options.welsh);
+    I.click(this.fields(party).documents.options.welsh);
 
-    I.waitForElement(this.fields(party).court.id);
-    await within(this.fields(party).court.id, () => {
-      I.click(this.fields(party).court.options.welsh);
-    });
-
-    I.waitForElement(this.fields(party).documents.id);
-    await within(this.fields(party).documents.id, () => {
-      I.click(this.fields(party).documents.options.welsh);
-    });
+    await I.clickContinue();
   },
 };
