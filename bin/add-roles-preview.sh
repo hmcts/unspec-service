261dbc3b72525faf0d062cbd14bf3f788fd86e0d
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2021-01-14 15:52:14 +0000

    added caseworker-caa role on preview

--- bin/add-roles-preview.sh
+++ bin/add-roles-preview.sh
@@ -2,6 +2,7 @@
 
 # User used during the CCD import and ccd-role creation
 ./civil-unspecified-docker/bin/utils/ccd-add-role.sh "caseworker-civil"
+./civil-unspecified-docker/bin/utils/ccd-add-role.sh "caseworker-caa"
 
 roles=("solicitor" "systemupdate")
 for role in "${roles[@]}"
