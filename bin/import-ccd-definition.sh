c7ef7922111c04394a19cd64f7982f2355e0da47
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-02-08 11:08:41 +0000

    check the vault values for ithc

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -4,4 +4,6 @@ definition_input_dir=$(realpath 'ccd-definition')
 definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-unspec-dev.xlsx"
 params="$@"
 
+echo "${CCD_CONFIGURER_IMPORTER_USERNAME}"
+echo "${CCD_CONFIGURER_IMPORTER_PASSWORD}"
 ./civil-unspecified-docker/bin/import-ccd-definition.sh "${definition_input_dir}" "${definition_output_file}" "${params}"
