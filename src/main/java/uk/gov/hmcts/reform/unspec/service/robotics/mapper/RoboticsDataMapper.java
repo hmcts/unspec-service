cd3d4cf1d2b3caa029355520f10dcf8c2d8050ab
Sabah Irfan <sabah.irfan@gmail.com>
2021-02-10 11:03:55 +0000

    fix null pointer

--- src/main/java/uk/gov/hmcts/reform/unspec/service/robotics/mapper/RoboticsDataMapper.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/service/robotics/mapper/RoboticsDataMapper.java
@@ -156,9 +156,14 @@ public class RoboticsDataMapper {
             .name(PartyUtils.getLitigiousPartyName(party, litigationFriend))
             .dateOfBirth(PartyUtils.getDateOfBirth(party).map(d -> d.format(ISO_DATE)).orElse(null))
             .addresses(addressMapper.toRoboticsAddresses(party.getPrimaryAddress()))
-            .solicitorOrganisationID(ofNullable(organisationPolicy)
-                                         .map(policy -> policy.getOrganisation().getOrganisationID())
-                                         .orElse(null))
+            .solicitorOrganisationID(getOrganisationID(organisationPolicy))
             .build();
+    }
+
+    private String getOrganisationID(OrganisationPolicy organisationPolicy) {
+        return ofNullable(organisationPolicy)
+            .map(policy -> ofNullable(policy.getOrganisation())
+                .map(org -> org.getOrganisationID()).orElse(null))
+            .orElse(null);
     }
 }
