f3b4a47f4a102bd74ca28a6926f43785fb33630f
Sabah Irfan <sabah.irfan@gmail.com>
2021-02-17 13:03:21 +0000

    Code cleanup

--- src/main/java/uk/gov/hmcts/reform/unspec/service/PaymentsService.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/service/PaymentsService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.unspec.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.payments.client.PaymentsClient;
 import uk.gov.hmcts.reform.payments.client.models.FeeDto;
