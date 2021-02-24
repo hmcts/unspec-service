b6ce1a3313444dbcf8690a670b8291b03db90895
Sam Walker <samw@kainos.com>
2021-02-24 10:46:29 +0000

    updated field names in fixtures

--- e2e/fixtures/events/informAgreeExtensionDate.js
+++ e2e/fixtures/events/informAgreeExtensionDate.js
@@ -3,16 +3,16 @@ const {date} = require('../../api/dataHelper');
 module.exports = {
   valid: {
     ExtensionDate: {
-      respondentSolicitor1claimResponseExtensionProposedDeadline: date(40)
+      respondentSolicitor1AgreedDeadlineExtension: date(40)
     }
   },
   invalid: {
     ExtensionDate: {
       past: {
-        respondentSolicitor1claimResponseExtensionProposedDeadline: date(-1)
+        respondentSolicitor1AgreedDeadlineExtension: date(-1)
       },
       beforeCurrentDeadline: {
-        respondentSolicitor1claimResponseExtensionProposedDeadline: date(10)
+        respondentSolicitor1AgreedDeadlineExtension: date(10)
       }
     }
   }
