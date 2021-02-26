17e5c350a684c2ffafb5b1d1d643cfc1db0a34d0
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2021-02-26 17:00:31 +0000

    updated EventHistoryMapperTest

--- src/test/java/uk/gov/hmcts/reform/unspec/service/robotics/mapper/EventHistoryMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/unspec/service/robotics/mapper/EventHistoryMapperTest.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.unspec.service.robotics.mapper;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,12 @@ import uk.gov.hmcts.reform.unspec.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.unspec.model.CaseData;
 import uk.gov.hmcts.reform.unspec.model.robotics.Event;
 import uk.gov.hmcts.reform.unspec.model.robotics.EventDetails;
+import uk.gov.hmcts.reform.unspec.model.robotics.EventHistory;
 import uk.gov.hmcts.reform.unspec.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.unspec.service.flowstate.FlowState;
 import uk.gov.hmcts.reform.unspec.service.flowstate.StateFlowEngine;
+
+import java.util.stream.Stream;
 
 import static java.time.format.DateTimeFormatter.ISO_DATE;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +28,8 @@ import static org.assertj.core.api.Assertions.assertThat;
     EventHistoryMapper.class
 })
 class EventHistoryMapperTest {
+
+    private static final Event EMPTY_EVENT = Event.builder().build();
 
     @Autowired
     EventHistoryMapper mapper;
@@ -40,16 +48,57 @@ class EventHistoryMapperTest {
                               .build())
             .build();
 
-        var events = mapper.buildEvents(caseData);
+        var eventHistory = mapper.buildEvents(caseData);
 
-        assertThat(events).isNotNull();
-        assertThat(events)
+        assertThat(eventHistory).isNotNull();
+        assertThat(eventHistory)
             .extracting("miscellaneous")
             .asList()
-            .hasSize(1);
-        assertThat(events)
-            .extracting("miscellaneous")
-            .asList()
+            .hasSize(1)
             .containsExactly(expectedEvent);
+        assertEmptyEvents(
+            eventHistory,
+            "acknowledgementOfServiceReceived",
+            "consentExtensionFilingDefence",
+            "defenceFiled",
+            "defenceAndCounterClaim",
+            "receiptOfPartAdmission",
+            "receiptOfAdmission",
+            "replyToDefence",
+            "directionsQuestionnaireFiled"
+        );
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = FlowState.Main.class, mode = EnumSource.Mode.EXCLUDE, names = {
+        "RESPONDENT_FULL_ADMISSION",
+        "RESPONDENT_PART_ADMISSION",
+        "RESPONDENT_COUNTER_CLAIM",
+        "PROCEEDS_OFFLINE_UNREPRESENTED_DEFENDANT"
+    })
+    void shouldBuildEmptyEventHistory_whenNoMappingsDefinedForStateFlow(FlowState.Main flowStateMain) {
+        CaseData caseData = CaseDataBuilder.builder().atState(flowStateMain).build();
+
+
+        var eventHistory = mapper.buildEvents(caseData);
+
+        assertThat(eventHistory).isNotNull();
+        assertEmptyEvents(
+            eventHistory,
+            "miscellaneous",
+            "acknowledgementOfServiceReceived",
+            "consentExtensionFilingDefence",
+            "defenceFiled",
+            "defenceAndCounterClaim",
+            "receiptOfPartAdmission",
+            "receiptOfAdmission",
+            "replyToDefence",
+            "directionsQuestionnaireFiled"
+        );
+    }
+
+    private void assertEmptyEvents(EventHistory eventHistory, String... eventNames) {
+        Stream.of(eventNames).forEach(
+            eventName -> assertThat(eventHistory).extracting(eventName).asList().containsOnly(EMPTY_EVENT));
     }
 }
