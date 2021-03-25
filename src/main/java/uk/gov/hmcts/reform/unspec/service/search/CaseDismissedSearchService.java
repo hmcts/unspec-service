9a432b35ecbeadf53aeb9451885ac7d5aadbf8ee
Sam Walker <samw@Sams-MacBook-Pro.local>
2021-03-25 12:56:17 +0000

    fixed es query with shoulds

--- src/main/java/uk/gov/hmcts/reform/unspec/service/search/CaseDismissedSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/unspec/service/search/CaseDismissedSearchService.java
@@ -21,28 +21,19 @@ public class CaseDismissedSearchService extends ElasticSearchService {
     //TODO: claimDismissedDeadline is not yet set anywhere. Date is 6 months
     // of no activity in state view and respond to defence which is currently AWAITING_CLAIMANT_INTENTION.
     public Query query(int startIndex) {
-        return new Query(boolQuery()
-                          .must(rangeQuery("data.claimNotificationDeadline").lt("now-1d")),
+        return new Query(
+            boolQuery()
+                .minimumShouldMatch(1)
+                .should(boolQuery()
+                          .must(rangeQuery("data.claimNotificationDeadline").lt("now"))
+                          .must(beState("AWAITING_CASE_NOTIFICATION")))
+                .should(boolQuery()
+                          .must(rangeQuery("data.claimDismissedDeadline").lt("now"))
+                          .must(beState("CREATED"))),
             List.of("reference"),
             startIndex
         );
     }
-//:TODO:Need to fix query not bringing back any cases
-
-//    public Query query(int startIndex) {
-//        return new Query(
-//            boolQuery()
-//                .minimumShouldMatch(1)
-//                .must(boolQuery()
-//                          .must(rangeQuery("data.claimNotificationDeadline").lt("now"))
-//                          .must(beState("AWAITING_CASE_NOTIFICATION")))
-//                .must(boolQuery()
-//                          .must(rangeQuery("data.claimDismissedDeadline").lt("now"))
-//                          .must(beState("CREATED"))),
-//            List.of("reference"),
-//            startIndex
-//        );
-//    }
 
     public BoolQueryBuilder beState(String state) {
         return boolQuery()
