da778977096ce8fda98001b9d8d528c664d16494
Sabah u Din Irfan <sabah.irfan@HMCTS.net>
2021-04-08 11:08:05 +0100

    correct key name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -50,7 +50,7 @@ data "azurerm_key_vault_secret" "send_grid_api_key" {
 
 
   key_vault_id = data.azurerm_key_vault.send_grid.id
-  name         = "hmcts-damages-key"
+  name         = "hmcts-damages-api-key"
 }
 
 resource "azurerm_key_vault_secret" "sendgrid_api_key" {
